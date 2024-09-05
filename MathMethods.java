7a392369bf761d57469c674b657fadc30cb10b3a
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-05 12:37:45 -0400

    assigned variables to math methods and printed values for testing

--- MathMethods.java
+++ MathMethods.java
@@ -9,12 +9,16 @@ public class MathMethods {
       double y = scnr.nextDouble();
       double z = scnr.nextDouble();
 
-      double input1;
-      double input2;
-      double input3;
-      double input4;
-      double sqrtValue;
-      
+      double input1 = Math.pow(x, z);
+      double input2 = Math.pow(x, Math.pow(y, z)); 
+      double input3 = Math.abs(y);
+      double sqrtValue = Math.pow(x * y, z);
+      double input4 = Math.sqrt(sqrtValue);
+      System.out.println(input1);
+      System.out.println(input2);
+      System.out.println(input3);
+      System.out.println(input4);
+      System.out.println(sqrtValue);
       scnr.close();
    }
 }
