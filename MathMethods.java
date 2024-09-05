0201d254cc8fb472f6b3890d36ffafdd8504ebed
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-05 12:24:33 -0400

    init file commit

---
+++ MathMethods.java
@@ -0,0 +1,13 @@
+package MathMethods;
+
+import java.util.Scanner;
+
+public class MathMethods {
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      double x;
+      double y;
+      double z;
+      /* Type your code here. */
+   }
+}
